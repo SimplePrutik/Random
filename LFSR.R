5c67c7c27b9dc5a923a0a58feb7bbc8072069c8b
SimplePrutik <dimodaktil@yandex.ru>
2015-07-23 12:23:04 +0300

    faster than previous

--- LFSR.R
+++ LFSR.R
@@ -1,40 +1,34 @@
 square <- function(x)
 {
-	y <- x*x
-	y
+	x*x
 }
 
 sum2 <- function (...)
 {
-	result <- sum(square(...))
-	sqrt(result)
+	sqrt(sum(square(...)))
 }
 
 Pi_LFSR <- function(seed, amount)
 {
 	res <- c()
 	g <- gl(amount, 2)
-	x <- intToBits(seed)
 	amount <- amount * 62
-	z <- c()
+	z <- 0
+	a <- 0
 	while (amount != 0)
 	{
-		z <- c(z,x[1])
-		y <- (x[1] == x[4])
-		seed <- bitwShiftR(seed, 1) + (2^30) * as.numeric(y)
-		x <- intToBits(seed)
-		if (length(z) == 31)
+		z <- z + (2^a)*bitwAnd(seed, 1) 					#charging of value
+		a <- a + 1
+		y <- as.logical(bitwAnd(seed, 1)) == as.logical(bitwAnd(seed, 2^3))	#polynomial x^31 + x^28
+		seed <- bitwShiftR(seed, 1) + (2^30)*y					#shift and computing of new number
+		if (a == 31)
 		{
-			z <- c(z, 0)
-			k <- as.raw(z)
-			k <- packBits(k, "integer")
-			res <- c(res, k / (2^31 - 1))
-			z <- c()
+			res <- c(res, z/(2^31))
+			a <- 0
+			z <- 0
 		}
 		amount <- amount - 1
 	}
 	x <- tapply(res, g, sum2)
-	P <- length(x[x < 1])/length(x)*4
-	P
-
+	length(x[x < 1])/length(x)*4
 }
