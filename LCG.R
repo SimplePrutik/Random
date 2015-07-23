c925e3004009ef7cbb106c300f590afd67c7eb2c
SimplePrutik <dimodaktil@yandex.ru>
2015-07-23 12:41:26 +0300

    added comments

--- LCG.R
+++ LCG.R
@@ -1,33 +1,30 @@
 square <- function(x)
 {
-	y <- x*x
-	y
+	x*x
 }
 
 sum2 <- function (...)
 {
-	result <- sum(square(...))
-	result <- sqrt(result)	
-	result
+	sqrt(sum(square(...)))
 }
+
+# ^ function which find point location ^
 
 Pi_LCG <- function (seed, length)
 {
 	f <- gl(length, 2)
 	length <- length * 2
-	res <- c()
-	A <- 23456757
-	C <- 54678657
-	M <- 2^32
+	res <- c()		
+	A <- 23456757				#
+	C <- 54678657				#
+	M <- 2^32				#constants	
 	while (length != 0)
 	{
-		seed <- (A * seed + C) %% M
-		t <- seed / M
-		res <- c(res,t)
-		length <- length - 1
+		seed <- (A * seed + C) %% M	#algorithm step
+		t <- seed / M			#computing value
+		res <- c(res,t)			#adding to coordinates vector 
+		length <- length - 1		
 	}
-	x <- tapply(res, f, sum2)
-	P <- length(x[x < 1])/length(x)*4
-	P
-
+	x <- tapply(res, f, sum2)		#applying of function 
+	length(x[x < 1])/length(x)*4		#computing of Pi
 }
