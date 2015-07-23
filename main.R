8ffcc764d6e84563c5ea8dd19573eb5e6bde01a5
SimplePrutik <dimodaktil@yandex.ru>
2015-07-23 12:53:30 +0300

    1234567890 replaced by constant

--- main.R
+++ main.R
@@ -3,5 +3,6 @@ source("LCG.R")
 source("LFSR.R")
 
 N <- 10000
+seed <- 1234567890
 
-print(c("Uniform" = Pi_unif(N), "LCG" = Pi_LCG(1234567890, N), "LFSR" = Pi_LFSR(1234567890, N)))
+print(c("Uniform" = Pi_unif(N), "LCG" = Pi_LCG(seed, N), "LFSR" = Pi_LFSR(seed, N)))
