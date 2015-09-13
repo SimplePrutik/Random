c363cbbe7bfd08f86c03a21b54e580d3ff234bdd
SimplePrutik <dimodaktil@yandex.ru>
2015-09-14 00:03:05 +0300

    optimized again

--- uniform.R
+++ uniform.R
@@ -1,3 +1,4 @@
+#required functions
 source("rqrd_func.R")
 
 #Monte-Carlo method
@@ -9,14 +10,6 @@ Pi_unif <- function (acc)			#acc - accuracy of test
 	length(x[x < 1])/length(x)*4		#computing of Pi
 }
 
-#function of increasing amount of points
-add <- function (x, amount)						#x - matrix, amount - number of increasing
-{
-	y <- matrix(runif(amount * 2, 0, 1), amount, 2)			#creating of matrix
-	z <- cbind(y, floor(y[,1] * y[,1] + y[,2] * y[,2]) + 1)		#creating of column meaning hit the circle for future functions
-	rbind(x,z)							#binding with original matrix
-}
-
 #function for graph
 Mersenne_Twister <- function (acc, interval)
 {
@@ -26,7 +19,7 @@ Mersenne_Twister <- function (acc, interval)
 	ani.options(interval = 0.1)			#interval between frames
 	while(nrow(m) < acc)				#loop of frames
 	{
-		m <- add(m, interval)			#enlarging of matrix
+		m <- add(m, interval, runif)		#enlarging of matrix
 		plot(m[,1],m[,2], col=m[,3], cex = 0.3, xlab = "", ylab = "")	#calling of graph 
 		panel.text(paste("Pi: ", round(sum((m[,3] - 2) * (-1)) / nrow(m) * 4, digits = 4)), x = 140, y = 40)	#output current Pi
 		panel.text(paste("Accuracy: ", nrow(m)), x = 300, y = 40)	#output current accuracy	
