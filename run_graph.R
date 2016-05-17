cfbd9583703c5cf4610bf119bcd771b3a2521c3f
SimplePrutik <SimplePrutik>
2016-05-17 11:46:39 +0300

    minor fix with ip

--- run_graph.R
+++ run_graph.R
@@ -7,31 +7,34 @@ if (!("shiny" %in% rownames(installed.packages())))
     
 library(shiny)
 
-shinyApp(ui = fluidPage
-    (
-        titlePanel("Monte-Carlo"),
-        sidebarLayout
-        ( 
-            sidebarPanel
-            (
-                sliderInput("bins",
-                    "Amount of points",
-                    min = 100,
-                    max = 10000,
-                    value = 1),
-                selectInput("var",
-                    label = "Choose generator",
-                    choices = list("Mersenne-Twister", "LCG", "LFSR"),
-                    selected = "Mersenne-Twister", selectize=TRUE)
-            ),
-            mainPanel
-            (
-                plotOutput("distPlot") 
-            )
-        )
-        
-    ), server = function(input, output) 
-{
-  
-  output$distPlot <- renderPlot( {web_Graph(input$var, input$bins)  }, width = 500, height = 500)
-}, options = list(launch.browser = TRUE))
+runApp(
+	appDir = shinyApp(ui = fluidPage
+		(
+		    titlePanel("Monte-Carlo"),
+		    sidebarLayout
+		    ( 
+		        sidebarPanel
+		        (
+		            sliderInput("bins",
+		                "Amount of points",
+		                min = 100,
+		                max = 10000,
+		                value = 1),
+		            selectInput("var",
+		                label = "Choose generator",
+		                choices = list("Mersenne-Twister", "LCG", "LFSR"),
+		                selected = "Mersenne-Twister", selectize=TRUE)
+		        ),
+		        mainPanel
+		        (
+		            plotOutput("distPlot") 
+		        )
+		    )
+		    
+		), server = function(input, output) 
+	{
+	  
+	  output$distPlot <- renderPlot( {web_Graph(input$var, input$bins)  }, width = 500, height = 500)
+	}, options = list(launch.browser = TRUE)),
+
+	host = "0.0.0.0")
